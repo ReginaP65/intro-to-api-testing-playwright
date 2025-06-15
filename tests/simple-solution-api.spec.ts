f4bd8b373fa7a0bf0d47daf0cbc8a67676757308
ReginaP65 <reginai.petrova@gmail.com>
2025-06-15 10:43:52 +0300

    adding negative scenarios

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test'
+ import { expect, test } from '@playwright/test'
 
 import { StatusCodes } from 'http-status-codes'
 
@@ -12,7 +12,7 @@ test('get order with correct id should receive code 200', async ({ request }) =>
   expect(response.status()).toBe(200)
 })
 
-test('post order with correct data should receive code 201', async ({ request }) => {
+test('post order with correct data should receive code 200', async ({ request }) => {
   // prepare request body
   const requestBody = {
     status: 'OPEN',
@@ -31,3 +31,23 @@ test('post order with correct data should receive code 201', async ({ request })
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
 })
+
+ test('post order with incorrect payload should receive code 400', async ({ request }) => {
+   // prepare request body
+   const requestBody = {
+     status: 'CLOSED',
+     courierId: 0,
+     customerName: 'string',
+     customerPhone: 'string',
+     comment: 'string',
+     id: 0,
+   }
+   // Send a POST request to the server
+   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+     data: requestBody,
+   })
+   // Log the response status and body
+   console.log('response status:', response.status())
+   console.log('response body:', await response.text())
+   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+ })
